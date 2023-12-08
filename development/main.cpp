ac12a9e1ff6ef40a40150ace5eb38741d6c0dd9b
Emil Kowalczyk <emilkowalczyk0@icloud.com>
2023-12-08 22:51:15 +0100

    add Car class and methods - test

--- development/main.cpp
+++ development/main.cpp
@@ -1,14 +1,33 @@
-//
-//  main.cpp
-//  development
-//
-//  Created by Emil Kowalczyk on 08/12/2023.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+class Car {
+public:
+    string mark;
+    string model;
+    int year;
+    int milage;
+    
+    void addCar() {
+        cout << "Mark: "; cin >> mark;
+        cout << "Model: "; cin >> model;
+        cout << "Year: "; cin >> year;
+        cout << "Milage: "; cin >> milage;
+    }
+    
+    void displayCarInfo() {
+        cout << "Mark" << mark << endl;
+        cout << "Model" << model << endl;
+        cout << "Year" << year << endl;
+        cout << "Milage" << milage << endl;
+    }
+};
+
+int main() {
+    Car car;
+    car.addCar();
+    car.displayCarInfo();
+    
     return 0;
 }
